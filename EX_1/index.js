040520b51e7501bad19c7c0cc026e221b42b07cc
frederico <portifolio.frederico@gmail.com>
2022-11-30 18:12:27 -0300

    first commit

---
+++ EX_1/index.js
@@ -0,0 +1,13 @@
+function send(){
+
+    let municipio = document.getElementById('total-eleitores').value;
+    let brancos = document.getElementById('votos-branco').value;
+    let nulos = document.getElementById('votos-nulos').value;
+    let validos = document.getElementById('votos-validos').value;
+
+    console.log(municipio);
+    console.log(brancos);
+    console.log(nulos);
+    console.log(validos);
+}
+
